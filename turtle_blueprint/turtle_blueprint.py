dc5316cd3d2ce9ce979fed8352a510636ab9eb77
Janani <vjanani021@gmail.com>
2023-04-24 14:32:00 +0530

    Day16

---
+++ turtle_blueprint/turtle_blueprint.py
@@ -0,0 +1,8 @@
+import turtle
+#https://docs.python.org/3/library/turtle.html#turtle.Screen
+#https://cs111.wellesley.edu/reference/colors
+timmy = turtle.Turtle()
+my_screen = turtle.Screen()
+timmy.shape("turtle")
+print(timmy.color("green"))
+my_screen.exitonclick()
